bc0bfd0c0f45495c46fd622f9b962b53866db189
Damir <dmr.marufjonov@gmail.com>
2026-02-26 00:22:09 +0500

    feat: implement responsive & functional review section

--- online-zoo/src/js/main.js
+++ online-zoo/src/js/main.js
@@ -2,7 +2,9 @@ import "normalize.css";
 import "../scss/main.scss";
 
 import { initPetsSlider } from "./slider";
+import { initReviewSlider } from "./slider";
 
 document.addEventListener("DOMContentLoaded", () => {
   initPetsSlider();
+  initReviewSlider();
 });
--- online-zoo/src/js/slider.js
+++ online-zoo/src/js/slider.js
@@ -2,27 +2,58 @@ import "normalize.css";
 import "../scss/main.scss";
 
 export const initPetsSlider = () => {
-  const cardWrapper = document.querySelector(".pets__wrapper");
+  const cardSlider = document.querySelector(".pets__slider");
   const cardList = document.querySelector(".pets__list");
   const card = document.querySelector(".pets__item");
-  const prevBtn = document.getElementById("prevBtn");
-  const nextBtn = document.getElementById("nextBtn");
+  const prevBtn = document.getElementById("pets-prev-btn");
+  const nextBtn = document.getElementById("pets-next-btn");
 
-  if (!cardWrapper || !cardList || !prevBtn || !nextBtn) return;
+  if (!cardSlider || !cardList || !prevBtn || !nextBtn) return;
 
   const cardWidth = card.offsetWidth;
   const cardGap = parseFloat(window.getComputedStyle(cardList).gap);
   const move = cardWidth + cardGap;
 
   const prev = () => {
-    cardWrapper.scrollBy({
+    cardSlider.scrollBy({
       left: -move,
       behavior: "smooth",
     });
   };
 
   const next = () => {
-    cardWrapper.scrollBy({
+    cardSlider.scrollBy({
+      left: move,
+      behavior: "smooth",
+    });
+  };
+
+  prevBtn.addEventListener("click", prev);
+  nextBtn.addEventListener("click", next);
+};
+
+export const initReviewSlider = () => {
+  const cardSlider = document.querySelector(".review__slider");
+  const cardList = document.querySelector(".review__list");
+  const card = document.querySelector(".review__item");
+  const prevBtn = document.getElementById("review-prev-btn");
+  const nextBtn = document.getElementById("review-next-btn");
+
+  if (!cardSlider || !cardList || !prevBtn || !nextBtn) return;
+
+  const cardWidth = card.offsetWidth;
+  const cardGap = parseFloat(window.getComputedStyle(cardList).gap);
+  const move = cardWidth + cardGap;
+
+  const prev = () => {
+    cardSlider.scrollBy({
+      left: -move,
+      behavior: "smooth",
+    });
+  };
+
+  const next = () => {
+    cardSlider.scrollBy({
       left: move,
       behavior: "smooth",
     });
