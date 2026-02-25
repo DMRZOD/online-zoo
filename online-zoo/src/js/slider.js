30482d047120aa5c7f9c9d666cc2dd03e87b1743
Damir <dmr.marufjonov@gmail.com>
2026-02-25 06:54:57 +0500

    feat: add js logic to the slider

--- online-zoo/src/js/slider.js
+++ online-zoo/src/js/slider.js
@@ -2,18 +2,30 @@ import "normalize.css";
 import "../scss/main.scss";
 
 export const initPetsSlider = () => {
-  const cardList = document.querySelector("pets__list");
+  const cardWrapper = document.querySelector(".pets__wrapper");
+  const cardList = document.querySelector(".pets__list");
+  const card = document.querySelector(".pets__item");
   const prevBtn = document.getElementById("prevBtn");
   const nextBtn = document.getElementById("nextBtn");
 
-  if (!cardList || !prevBtn || !nextBtn) return;
+  if (!cardWrapper || !cardList || !prevBtn || !nextBtn) return;
+
+  const cardWidth = card.offsetWidth;
+  const cardGap = parseFloat(window.getComputedStyle(cardList).gap);
+  const move = cardWidth + cardGap;
 
   const prev = () => {
-    console.log(prevBtn);
+    cardWrapper.scrollBy({
+      left: -move,
+      behavior: "smooth",
+    });
   };
 
   const next = () => {
-    console.log(nextBtn);
+    cardWrapper.scrollBy({
+      left: move,
+      behavior: "smooth",
+    });
   };
 
   prevBtn.addEventListener("click", prev);
