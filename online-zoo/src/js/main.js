c6e30410a9f2a4680e6b87bfae31b02105d57c92
Damir <dmr.marufjonov@gmail.com>
2026-03-11 03:57:03 +0500

    refactor: refactor and optimized code for better readability

--- online-zoo/src/js/main.js
+++ online-zoo/src/js/main.js
@@ -1,18 +1,8 @@
 import "normalize.css";
 import "../scss/main.scss";
 
-import { initPetsSlider } from "./slider";
-import { initReviewSlider } from "./slider";
-import { initLiveViewsSlider } from "./slider";
 import { initMobileMenu } from "./menu";
-import { initSidebar } from "./sidebar";
-import { initDonationPopup } from "./popup";
 
 document.addEventListener("DOMContentLoaded", () => {
   initMobileMenu();
-  initPetsSlider();
-  initReviewSlider();
-  initLiveViewsSlider();
-  initSidebar();
-  initDonationPopup();
 });
